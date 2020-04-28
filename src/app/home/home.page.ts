339ff0a33ecd60b78e1d8c8aa239bd5e9572eb3c
Jesus bossa <x@jesusbossa.com>
2020-04-28 10:27:24 -0500

    [ADD] Build to replicate the edit image error

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,5 @@
+import { Plugins, CameraResultType, CameraSource } from '@capacitor/core';
+import { AlertController } from '@ionic/angular';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,7 +8,49 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(private alertController: AlertController) {}
 
-  constructor() {}
-
+  public async uploadImage() {
+    const alert = await this.alertController.create({
+      header: 'Hi',
+      message: 'Want to upload an image?.',
+      buttons: [
+        {
+          text: 'Camera',
+          handler: async () => {
+            try {
+              const image = await Plugins.Camera.getPhoto({
+                quality: 100,
+                allowEditing: true,
+                saveToGallery: false,
+                resultType: CameraResultType.DataUrl,
+                source: CameraSource.Camera,
+              });
+              console.log(image);
+              // this.handleDataUrl(image.dataUrl);
+            } catch (error) {
+              console.log(error);
+            }
+          },
+        },
+        {
+          text: 'Gallery',
+          handler: async () => {
+            try {
+              const image = await Plugins.Camera.getPhoto({
+                quality: 100,
+                allowEditing: true,
+                resultType: CameraResultType.DataUrl,
+                source: CameraSource.Photos,
+              });
+              console.log(image);
+            } catch (error) {
+              console.log(error);
+            }
+          },
+        },
+      ],
+    });
+    await alert.present();
+  }
 }
